49787d1f96ea2d6cde6afaab39f68a816f2d5ea7
honapay <honami.gadget@gmail.com>
2020-06-14 14:58:14 +0900

    enum対応

--- database/migrations/2020_06_14_123757_create_cicinquiry_table.php
+++ database/migrations/2020_06_14_123757_create_cicinquiry_table.php
@@ -26,8 +26,8 @@ class CreateCicinquiryTable extends Migration
             $table->integer('phoneId_1'); //6.電話番号ID(1)
             $table->integer('phoneId_2'); //7.電話番号ID(2)
             $table->integer('addressId'); //5.住所ID 
-            $table->integer('applyInquiryTypeId'); //9.照会区分
-            $table->integer('applyContractTypeId'); //10.申込区分
+            $table->enum('applyInquiryType',['契約者','保証人']); //9.照会区分
+            $table->enum('applycontractType',['カード等','個品割賦','リース','保証契約','無保証融資','保証融資','住宅ローン']); //10.申込区分
             $table->integer('applyAmount'); //11.契約予定額
             $table->ingeger('applyCounts'); //12.支払予定回数
             $table->integer('applyProductTypeId_1'); //13.商品名(1)ID
@@ -39,7 +39,7 @@ class CreateCicinquiryTable extends Migration
             $table->string('applyLength_3'); //19. 数量・回数・期間(3)
             
             // columns for use
-            $table->integer('usePurposeId'); //6.利用目的
+            $table->enum('usePurpose',['消費者対応','新規再照会','途上与信','法定途上与信','配偶者再照会','再照会']); //6.利用目的
             $table->timestamps();
         });
     }
